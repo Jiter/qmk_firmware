7422b6ee84a4b8d38407fe8734a0b0600f6660f0
Jiter <davibloch@googlemail.com>
2020-05-01 21:42:55 +0200

    NEO2 Enable flag set

--- keyboards/xd87/rules.mk
+++ keyboards/xd87/rules.mk
@@ -31,6 +31,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
+NEO2_ENABLE = yes
 
 # Supported layouts
 LAYOUTS = tkl_ansi tkl_iso
